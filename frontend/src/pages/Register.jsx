c32d5aea80253bb57ac9a191cdc770200802e6e4
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-05-05 22:12:14 +0200

    Update Register form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { FaUser } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
@@ -21,7 +20,7 @@ function Register() {
   const { data, error, mutate, isLoading, isError, isSuccess } = useMutation(
     (variables) => register(variables)
   );
-  
+
   const user = getUser();
 
   useEffect(() => {
@@ -74,72 +73,101 @@ function Register() {
   }
 
   return (
-    <>
-      <section className="heading">
-        <h1>
-          <FaUser /> Register
-        </h1>
-        <p>Please create an account</p>
-      </section>
+    <div className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Register your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={onSubmit}>
+              <div>
+                <label
+                  for="name"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your name
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
+                  required={true}
+                  placeholder="John Doe"
+                  onChange={onChange}
+                />
+              </div>
 
-      <section className="form">
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <input
-              className="form-control"
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              placeholder="Enter your name"
-              onChange={onChange}
-            />
-          </div>
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  id="email"
+                  name="email"
+                  value={email}
+                  type="email"
+                  required={true}
+                  placeholder="name@company.com"
+                  onChange={onChange}
+                />
+              </div>
 
-          <div className="form-group">
-            <input
-              className="form-control"
-              id="email"
-              name="email"
-              value={email}
-              type="email"
-              placeholder="Enter your email"
-              onChange={onChange}
-            />
-          </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your password
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  id="password"
+                  name="password"
+                  value={password}
+                  type="password"
+                  required={true}
+                  placeholder="••••••••"
+                  onChange={onChange}
+                />
+              </div>
 
-          <div className="form-group">
-            <input
-              className="form-control"
-              id="password"
-              name="password"
-              value={password}
-              type="password"
-              placeholder="Enter your password"
-              onChange={onChange}
-            />
-          </div>
+              <div>
+                <label
+                  for="password2"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Repeat password
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  id="password2"
+                  name="password2"
+                  value={password2}
+                  type="password"
+                  required={true}
+                  placeholder="••••••••"
+                  onChange={onChange}
+                />
+              </div>
 
-          <div className="form-group">
-            <input
-              className="form-control"
-              id="password2"
-              name="password2"
-              value={password2}
-              type="password"
-              placeholder="Confirm your password"
-              onChange={onChange}
-            />
+              <button
+                type="submit"
+                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Submit
+              </button>
+            </form>
           </div>
-
-          <div className="form-group">
-            <button type="submit" className="btn btn-block">
-              Submit
-            </button>
-          </div>
-        </form>
-      </section>
-    </>
+        </div>
+      </div>
+    </div>
   );
 }
 
