c815c3e6372aeafe1d2559ecc21d16e625b75e3c
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-05-05 23:08:24 +0200

    Add basic styles for menu

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,7 +1,11 @@
 function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <div className="bg-gray-50 dark:bg-gray-900">
+      <div className="justify-center px-6  mx-auto md:h-screen lg:py-0 text-white">
+        Menu
+      </div>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
