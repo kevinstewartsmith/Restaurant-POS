7c5ce2090f203ddc6425c1bf12c831034ffc64fe
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-05-05 23:09:00 +0200

    Add basic styles for menu items

--- frontend/src/pages/MenuItems.jsx
+++ frontend/src/pages/MenuItems.jsx
@@ -1,7 +1,11 @@
 function MenuItems() {
   return (
-    <div>MenuItems</div>
-  )
+    <div className="bg-gray-50 dark:bg-gray-900">
+      <div className="justify-center px-6  mx-auto md:h-screen lg:py-0 text-white">
+        Menu Items
+      </div>
+    </div>
+  );
 }
 
-export default MenuItems
+export default MenuItems;
