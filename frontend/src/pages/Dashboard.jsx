af6154e92a6077c227295059dd699699fb4f6808
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-05-06 14:37:25 +0200

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { useQuery } from "react-query";
@@ -6,11 +6,13 @@ import { getRestaurants } from "../helper/restaurant";
 import { getUser } from "../helper/user";
 import Restaurant from "../components/restaurant/Restaurant";
 import RestaurantForm from "../components/restaurant/RestaurantForm";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 function Dashboard() {
   const navigate = useNavigate();
 
   const user = getUser();
+  const [showForm, setShowForm] = useState("");
 
   const { isLoading, data } = useQuery("restaurants", () =>
     getRestaurants(user.token)
@@ -26,6 +28,14 @@ function Dashboard() {
     return <Spinner />;
   }
 
+  const displayForm = () => {
+    if (showForm) {
+      setShowForm(0);
+    } else {
+      setShowForm(1);
+    }
+  };
+
   return (
     <div className="bg-gray-50 dark:bg-gray-900">
       <div className="justify-center px-6 lg:py-0 text-white">
@@ -36,12 +46,19 @@ function Dashboard() {
         </div>
 
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <RestaurantForm />
+          <button onClick={() => displayForm()}>
+            {showForm ? (
+              <AiOutlineMinusCircle size={50} />
+            ) : (
+              <AiOutlinePlusCircle size={50} />
+            )}
+          </button>
+          {showForm ? <RestaurantForm /> : ""}
         </div>
 
         <div className="sm:p-8">
           {data.length > 0 ? (
-            <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
               {data.map((restaurant) => (
                 <Restaurant key={restaurant._id} restaurant={restaurant} />
               ))}
