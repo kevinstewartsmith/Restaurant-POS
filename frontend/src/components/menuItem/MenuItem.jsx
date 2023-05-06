3cbdb0265d5d052ee7d49fc9db0bfdfc8528c709
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-05-06 14:37:23 +0200

    Update MenuItem.jsx

--- frontend/src/components/menuItem/MenuItem.jsx
+++ frontend/src/components/menuItem/MenuItem.jsx
@@ -17,7 +17,7 @@ function MenuItem({ menuItem }) {
   }
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg shadow-gray-700">
       <img
         className="w-full"
         src={menuItem.photo ? menuItem.photo : "boilerplate-image.jpg"}
