bdc4adf3f6d2f910db736458240e6dd05d04b94e
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-05-06 22:55:50 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
 import Menu from "./pages/Menu";
 import MenuItems from "./pages/MenuItems";
+import Tables from "./pages/Tables";
 
 function App() {
   return (
@@ -15,11 +16,12 @@ function App() {
         <div className="bg-gray-900">
           <Header />
           <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/menu' element={<Menu />} />
-            <Route path='/items' element={<MenuItems />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/items" element={<MenuItems />} />
+            <Route path="/tables" element={<Tables />} />
           </Routes>
         </div>
         <ToastContainer />
