790dcd67552d4c59f2915cb5b96092ed71f690eb
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-05-06 13:35:09 +0200

    Add proper menu handling

--- frontend/src/components/menu/MenuForm.jsx
+++ frontend/src/components/menu/MenuForm.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { createMenu } from "../../helper/menu";
-import { useQueryClient, useMutation } from "react-query";
+import { useQueryClient, useMutation, useQuery } from "react-query";
 import Spinner from "../Spinner";
+import { getRestaurants } from "../../helper/restaurant";
+import { getUser } from "../../helper/user";
+import { toast } from "react-toastify";
 
 function MenuForm() {
   const [name, setName] = useState("");
@@ -17,16 +20,24 @@ function MenuForm() {
     },
   });
 
+  const user = getUser();
+  const { data, loading: queryLoading } = useQuery("restaurants", () =>
+    getRestaurants(user.token)
+  );
+
   const onSubmit = (e) => {
     e.preventDefault();
-
-    mutate({ name, description, restaurant });
-    setName("");
-    setDescription("");
-    setRestaurant("");
+    if (name && description && restaurant) {
+      mutate({ name, description, restaurant });
+      setName("");
+      setDescription("");
+      setRestaurant("");
+    } else {
+      toast.error("Please fill all required data");
+    }
   };
 
-  if (isLoading) {
+  if (isLoading || queryLoading) {
     return <Spinner />;
   }
 
@@ -47,6 +58,7 @@ function MenuForm() {
               name="name"
               id="name"
               value={name}
+              required={true}
               onChange={(e) => setName(e.target.value)}
             />
           </div>
@@ -63,6 +75,7 @@ function MenuForm() {
               type="description"
               name="description"
               id="description"
+              required={true}
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
@@ -71,18 +84,31 @@ function MenuForm() {
           <div>
             <label
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              htmlFor="text"
+              htmlFor="resturant"
             >
               Restaurant
             </label>
-            <input
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              type="restaurant"
-              name="restaurant"
-              id="restaurant"
-              value={restaurant}
-              onChange={(e) => setRestaurant(e.target.value)}
-            />
+
+            {data && data.length > 0 ? (
+              <select
+                onChange={(e) => setRestaurant(e.target.value)}
+                name="restaurant"
+                id="restaurant"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option>Select Menu</option>
+                {data.map((restaurant) => (
+                  <option key={restaurant._id} value={restaurant._id}>
+                    {restaurant.name}
+                  </option>
+                ))}
+              </select>
+            ) : (
+              <h3 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                You do not have any restaurants - please create restaurant
+                before creating menu.
+              </h3>
+            )}
           </div>
 
           <button
